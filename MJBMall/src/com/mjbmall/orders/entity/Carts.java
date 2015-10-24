a051248369c451ab9c70178c79514ded83d66e5f
ZZ <ZZ@mail.com>
2015-10-25 02:40:45 +0800

    123

---
+++ MJBMall/src/com/mjbmall/orders/entity/Carts.java
@@ -0,0 +1,48 @@
+package com.mjbmall.orders.entity;
+
+import java.math.BigDecimal;
+
+public class Carts {
+	private int goods_id;
+	private String name;
+	private BigDecimal price;
+	private String big_pic; 
+	private BigDecimal mktprice;
+	private String addTime;
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goodsId) {
+		goods_id = goodsId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public String getBig_pic() {
+		return big_pic;
+	}
+	public void setBig_pic(String bigPic) {
+		big_pic = bigPic;
+	}
+	public BigDecimal getMktprice() {
+		return mktprice;
+	}
+	public void setMktprice(BigDecimal mktprice) {
+		this.mktprice = mktprice;
+	}
+	public String getAddTime() {
+		return addTime;
+	}
+	public void setAddTime(String addTime) {
+		this.addTime = addTime;
+	}
+}
