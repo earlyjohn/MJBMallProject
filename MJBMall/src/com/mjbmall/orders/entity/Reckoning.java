d1b36fe717d3c769a100daec43633a6dcf669531
ZZ <ZZ@mail.com>
2015-10-25 16:17:37 +0800

    1

---
+++ MJBMall/src/com/mjbmall/orders/entity/Reckoning.java
@@ -0,0 +1,25 @@
+package com.mjbmall.orders.entity;
+
+public class Reckoning {
+     private int user_id;
+     private int[] goodsId;
+     private int reckoning;
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int userId) {
+		user_id = userId;
+	}
+	public int[] getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(int[] goodsId) {
+		this.goodsId = goodsId;
+	}
+	public int getReckoning() {
+		return reckoning;
+	}
+	public void setReckoning(int reckoning) {
+		this.reckoning = reckoning;
+	}
+}
