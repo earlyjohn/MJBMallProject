b3d6fb6f53d108ab3617cac0f84d2a21d36f873f
malw1 <malw1@yonyou.com>
2015-11-01 03:11:06 +0800

    1、个人信息
    Signed-off-by: malw1 <malw1@yonyou.com>

--- MJBMallAPP/js/changeinformationCtrl.js
+++ MJBMallAPP/js/changeinformationCtrl.js
@@ -4,7 +4,25 @@
         var html = '<div class="navbar-inner"><a class="left"><i class="icon spxq_icon-navbar"></i></a>'
             +'<div class="center">修改基本资料</div><div class="right editCart"><a class="link link-u saveBtn" style="color: white">保存</a></div></div>';
         $$('#changeinformationNavbar').html(html);
-
+        var user_id = window.localStorage.getItem("userId");
+        debugger;
+        uzu.rest.getJSON("/members/findMembers", { 'member_id': 10 }, function (data) {
+            debugger;
+            // 渲染模板
+            var context = {};
+            context = data.membersList[0];
+            if (context.email === "demo@demo.demo") {
+                context.sexFlag = false;
+            } else {
+                context.sexFlag = true;
+            }
+            var cartsListTemplate = $$('#memberInformationTpl').html();
+            var compiledCartsListTemplate = Template7.compile(cartsListTemplate);
+            var html = compiledCartsListTemplate(context);
+            $$('#memberInformation').html(html);
+            debugger;
+           
+        });
         $$('.saveBtn').on('click', function () {
             var userName = $$("#userName").val();
             var sex = $$("#sex").val();
