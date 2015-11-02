f0d276e88e436687917f0bb72169c393a115b249
ZZ <ZZ@qq.com>
2015-11-02 19:11:31 +0800

    1

--- MJBMall/src/com/mjbmall/orders/controller/MessageController.java
+++ MJBMall/src/com/mjbmall/orders/controller/MessageController.java
@@ -9,15 +9,49 @@ import com.mjbmall.orders.entity.Message;
 import com.opensymphony.xwork2.Action;
 
 public class MessageController {
-	Map result = new HashMap();
+	Map<String,Object> result = new HashMap<String,Object>();
 	private MessageMapper messageMapper;
 	private int recv_id=0;
+	private int send_id=0;
+	private String msg="";
+	private int status=1;
+	private int type=1;
+	/**
+	 * @param status 要设置的 status
+	 */
+	public void setStatus(int status) {
+		this.status = status;
+	}
 	public String findNewMsg(){
 		try{
 			Message m = new Message();
 			m.setRecv_id(recv_id);
+			m.setSend_id(send_id);
+			m.setStatus(status);
+			m.setType(type);
 			result.clear();
-			messageMapper.getNewMsg(m);
+			List<Message> list = messageMapper.getNewMsg(m);
+			int[] ids = new int[list.size()];
+			for(int i=0;i<list.size();i++){
+				ids[i]=list.get(i).getMessage_id();
+			}
+			messageMapper.updateMsgStatus(ids);
+			result.put("msgList", list);
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+		return Action.SUCCESS;
+	}
+	public String addMsg(){
+		try{
+			Message m = new Message();
+			m.setRecv_id(recv_id);
+			m.setSend_id(send_id);
+			m.setMsg(msg);
+			m.setType(type);
+			messageMapper.addMsg(m);
 			
 			
 		}catch(Exception e){
@@ -32,7 +66,22 @@ public class MessageController {
 	public void setRecv_id(int recvId) {
 		recv_id = recvId;
 	}
-	public Map getResult() {
+	public Map<String,Object> getResult() {
 		return result;
+	}
+	/**
+	 * @param send_id 要设置的 send_id
+	 */
+	public void setSend_id(int send_id) {
+		this.send_id = send_id;
+	}
+	/**
+	 * @param msg 要设置的 msg
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public void setType(int type) {
+		this.type = type;
 	}
 }
