003817db4758db675c1e9dac72cdb76b25895d6b
ZZ <ZZ@mail.com>
2015-11-08 17:02:40 +0800

    1

---
+++ MJBMall/src/com/mjbmall/goods/entity/CrlAvg.java
@@ -0,0 +1,23 @@
+package com.mjbmall.goods.entity;
+/**
+ * 轮播广告实体类
+ * @author Administrator
+ *
+ */
+public class CrlAvg {
+	private int goods_id;
+	private String pic;
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goodsId) {
+		goods_id = goodsId;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+}
---
+++ MJBMall/src/com/mjbmall/goods/entity/TypeOrderBy.java
@@ -0,0 +1,24 @@
+package com.mjbmall.goods.entity;
+
+public class TypeOrderBy {
+	private int orderBy;
+	
+	private int type;
+
+	public void setOrderBy(int orderBy) {
+		this.orderBy = orderBy;
+	}
+
+	public int getOrderBy() {
+		return orderBy;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getType() {
+		return type;
+	}
+  
+}
