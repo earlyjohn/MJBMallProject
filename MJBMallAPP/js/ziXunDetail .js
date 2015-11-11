705999d70428be8c59d63db2cb3b0a69977b3c33
xuejx <zhaluo_xjx@163.com>
2015-11-11 18:53:06 +0800

    f

---
+++ MJBMallAPP/js/ziXunDetail .js
@@ -0,0 +1,13 @@
+﻿var ziXunDetailCtrl = {
+    init: function (e) {
+        // 顶部导航
+        var html = "<div class='navbar-inner'><div class='left'><i class='icon search_icon-navbar'></i>"
+              + "</div><div class='center'>资讯详情页</div><div class='right'><i class='icon'></i></div></div>";
+        $$('#contentListNavbar').html(html);
+    
+        $$('.search_icon-navbar').on('click', function () {
+            mainView.router.back();
+        });
+    }
+};
+
