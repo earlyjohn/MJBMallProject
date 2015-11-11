0c8ce021bde918576fe448ef5af5651a9f11a954
ZZ <ZZ@qq.com>
2015-11-11 15:07:25 +0800

    1

--- MJBMall/src/com/mjbmall/goods/entity/Evaluate.java
+++ MJBMall/src/com/mjbmall/goods/entity/Evaluate.java
@@ -3,6 +3,9 @@ package com.mjbmall.goods.entity;
 public class Evaluate {
 	private int goods_id;
 	private String evaluate;
+	private String username;
+	private int star;
+	private String pic_url;
 	public int getGoods_id() {
 		return goods_id;
 	}
@@ -14,6 +17,24 @@ public class Evaluate {
 	}
 	public void setEvaluate(String evaluate) {
 		this.evaluate = evaluate;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public String getPic_url() {
+		return pic_url;
+	}
+	public void setPic_url(String pic_url) {
+		this.pic_url = pic_url;
 	}
 	
 
