6c4ab71667e180537e20d0901796fa8adfcddf1e
ZZ <ZZ@qq.com>
2015-11-13 11:33:49 +0800

    1

---
+++ MJBMall/src/com/mjbmall/members/entity/HeadPicUrl.java
@@ -0,0 +1,19 @@
+package com.mjbmall.members.entity;
+
+public class HeadPicUrl {
+	private int member_id;
+	private String head_pic;
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMemeber_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public String getHead_pic() {
+		return head_pic;
+	}
+	public void setHead_pic(String head_pic) {
+		this.head_pic = head_pic;
+	}
+
+}
