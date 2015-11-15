7ba428c5b889f42d472c69e667e10039cd539ccf
thinklock <13661199589>
2015-11-15 22:35:02 +0800

    pingjia aijjai

--- MJBMallAPP/js/publishEvaluateCtrl.js
+++ MJBMallAPP/js/publishEvaluateCtrl.js
@@ -22,6 +22,7 @@ var publishEvaluateCtrl = {
             mainView.router.back();
         });
 
+        var star=0;
         // 根据订单id得到该订单内的商品详情
         uzu.rest.getJSON("orders/findGoodsByOrderId", { 'order_id': order_id }, function (result) {
             // 渲染模板
@@ -34,17 +35,45 @@ var publishEvaluateCtrl = {
             goods_id = result.result.goodsList[0].goods_id;
         });
 
+        for(var i=0;i<5;i++){
+            (function(){
+                var p = i;
+                document.getElementById("pj_pic"+(p+1)).onclick = function() {
+                    getdefault();
+                    for(var j=0;j<=p;j++){
+                        document.getElementById("pj_pic"+(j+1)).src = "img/pjdj_1.png"
+                    }
+                    star = p+1;
+                    //alert(star);
+                }
+            })();
+        }
+
+        function getdefault(){
+            for(var i=0;i<5;i++){
+                (function(){
+                    var p = i;
+                    document.getElementById("pj_pic"+(p+1)).src = "img/pjdj_2.png"
+                })();
+            }
+        }
         document.getElementById("submit").onclick = function(){
             var content = document.getElementById("content").value;
             if(content == "请输入内容"){
-                myApp.alert("请输入您要评价的内容", "error")
+                myApp.alert("请输入您要评价的内容", "error");
                 return;
             }
-            uzu.rest.getJSON("evaluate/addEvaluate",{'user_id':user_id,'goods_id':goods_id,'evaluate_content':content,'star':'3'},function(){
+            if(star == 0){
+                myApp.alert("请输入您要评价的星级", "error");
+                return;
+            }
+            uzu.rest.getJSON("evaluate/addEvaluate",{'user_id':user_id,'goods_id':goods_id,'evaluate_content':content,'star':star},function(){
                 myApp.alert("成功，谢谢您的评价！", "success");
                 mainView.router.loadPage("order.html");
             });
         }
+
+
     }
 };
 
